1840f91b7e2445a8178be015cb29740bcd096bec
Dan Garland <dan@dangarland.co.uk>
2020-04-28 23:19:25 +0100

    Disabling buttons if the video is already in the cache

---
+++ src/hooks/cache.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+const useCache = () => {
+  const [cached, setCached] = useState([])
+
+  useEffect(() => {
+    caches
+      .open("video-cache")
+      .then(cache => cache.keys())
+      .then(keys => keys.map(req => req.url))
+      .then(urls => setCached(urls))
+  }, [cached])
+
+  return cached
+}
+
+export default useCache

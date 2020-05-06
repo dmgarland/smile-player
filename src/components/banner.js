31d3f5491a54c1ce38cd1533ca1c4f1db57157d2
Dan Garland <dan@dangarland.co.uk>
2020-05-06 22:25:47 +0100

    Rigging up install button

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,32 +1,26 @@
-import React, { useState } from "react"
+import React from "react"
 import { Box, IconButton } from "gestalt"
 
-const Banner = ({ children }) => {
-  const [visible, setVisible] = useState(true)
-
-  return (
-    visible && (
-      <Box
-        alignItems="center"
-        display="flex"
-        height={60}
-        color="blue"
-        rounding="pill"
-        paddingY={3}
-        margin={1}
-      >
-        <Box paddingX={1}>
-          <IconButton
-            icon="clear"
-            iconColor="white"
-            onClick={() => setVisible(false)}
-            size="xl"
-          />
-        </Box>
-        {children}
-      </Box>
-    )
-  )
-}
+const Banner = ({ setVisible, children }) => (
+  <Box
+    alignItems="center"
+    display="flex"
+    height={60}
+    color="blue"
+    rounding="pill"
+    paddingY={3}
+    margin={1}
+  >
+    <Box paddingX={1}>
+      <IconButton
+        icon="clear"
+        iconColor="white"
+        onClick={() => setVisible(false)}
+        size="xl"
+      />
+    </Box>
+    {children}
+  </Box>
+)
 
 export default Banner
--- src/components/install-banner.js
+++ src/components/install-banner.js
@@ -1,18 +1,54 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Banner from "./banner"
 import { Sticky, Text, Button, Box } from "gestalt"
 
-const InstallBanner = () => (
-  <Sticky top={0}>
-    <Banner>
-      <Box marginRight={6} flex="grow">
-        <Text color="white">Enjoy your songs offline using our free app!</Text>
-      </Box>
-      <Box paddingX={3}>
-        <Button text="Install" inline />
-      </Box>
-    </Banner>
-  </Sticky>
-)
+const InstallBanner = () => {
+  const [installEvent, setInstallEvent] = useState(null)
+  const [visible, setVisible] = useState(true)
+
+  const saveInstallEvent = e => {
+    e.preventDefault()
+    setInstallEvent(e)
+    setVisible(true)
+  }
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", saveInstallEvent)
+    return () =>
+      window.removeEventListener("beforeinstallprompt", saveInstallEvent)
+  }, [])
+
+  return (
+    visible && (
+      <div id="install-banner">
+        <Sticky top={0}>
+          <Banner setVisible={setVisible}>
+            <Box marginRight={6} flex="grow">
+              <Text color="white">
+                Enjoy your songs offline using our free app!
+              </Text>
+            </Box>
+            <Box paddingX={3}>
+              <Button
+                text="Install"
+                inline
+                onClick={e => {
+                  installEvent.prompt()
+                  installEvent.userChoice.then(choice => {
+                    if (choice.outcome === "accepted") {
+                      console.log("accepted")
+                    } else {
+                      console.log("rejected")
+                    }
+                  })
+                }}
+              />
+            </Box>
+          </Banner>
+        </Sticky>
+      </div>
+    )
+  )
+}
 
 export default InstallBanner

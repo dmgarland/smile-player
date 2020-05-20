421a74b6ddd3a1bc278f27ed91becb8b12c4c191
Dan Garland <dan@dangarland.co.uk>
2020-05-20 23:36:56 +0100

    Adding a donations button

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,91 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
-import { Box, Heading, Image } from "gestalt"
+import React, { useState } from "react"
+import {
+  Box,
+  Heading,
+  Image,
+  IconButton,
+  Layer,
+  Flyout,
+  Text,
+  Button
+} from "gestalt"
 import sunshine from "../images/sunshine.svg"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <Link
-      to="/"
-      style={{
-        textDecoration: `none`,
-        display: "block"
-      }}
-    >
-      <Box paddingY={6} display="flex" alignItems="center">
-        <img src={sunshine} id="logo" alt="Smiling Remotely" />
-        <Heading align="center" accessibilityLevel={1}>
-          {siteTitle}
-        </Heading>
+const Header = ({ siteTitle }) => {
+  const [donateVisible, setDonateVisible] = useState(false)
+  const anchorRef = React.useRef()
+
+  return (
+    <header>
+      <Box
+        color="white"
+        display="flex"
+        justifyContent="end"
+        alignItems="start"
+        paddingX="2"
+      >
+        <Box display="inlineBlock" ref={anchorRef}>
+          <IconButton
+            accessibilityLabel="Love Us? Donate!"
+            icon="heart"
+            iconColor="red"
+            size="lg"
+            accessibilityExpanded={!!donateVisible}
+            accessibilityHaspopup
+            onClick={() => {
+              setDonateVisible(!donateVisible)
+            }}
+          />
+          {donateVisible && (
+            <Layer>
+              <Flyout
+                anchor={anchorRef.current}
+                idealDirection="down"
+                onDismiss={() => setDonateVisible(false)}
+                positionRelativeToAnchor={true}
+                size="md"
+              >
+                <Box
+                  padding={3}
+                  display="flex"
+                  alignItems="center"
+                  direction="column"
+                  column={12}
+                >
+                  <Text align="center" weight="bold">
+                    Enjoying our videos? Please donate to help support us to
+                    bring new songs to more care homes near you.
+                  </Text>
+                  <Box paddingX={2} marginTop={3}>
+                    <a href="https://www.paypal.me/shapeshifterprod">
+                      <Button color="blue" text="Donate via PayPal" />
+                    </a>
+                  </Box>
+                </Box>
+              </Flyout>
+            </Layer>
+          )}
+        </Box>
       </Box>
-    </Link>
-  </header>
-)
+      <Link
+        to="/"
+        style={{
+          textDecoration: `none`,
+          display: "block"
+        }}
+      >
+        <Box paddingY={6} display="flex" alignItems="center">
+          <img src={sunshine} id="logo" alt="Smiling Remotely" />
+          <Heading align="center" accessibilityLevel={1}>
+            {siteTitle}
+          </Heading>
+        </Box>
+      </Link>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string

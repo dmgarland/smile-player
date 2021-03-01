43cd8492573b37e01bec14163a3990ff05b556af
Dan Garland <dan@dangarland.co.uk>
2021-03-01 22:39:39 +0000

    Install banner doens't need to be sticky

--- src/components/install-banner.js
+++ src/components/install-banner.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react"
 import Banner from "./banner"
-import { Container, Sticky, Text, Button, Box } from "gestalt"
+import { Container, Text, Button, Box } from "gestalt"
 // import { trackCustomEvent } from "gatsby-plugin-gtag"
 
 const InstallBanner = () => {
   const [installEvent, setInstallEvent] = useState(null)
-  const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(true)
 
   const saveInstallEvent = e => {
     e.preventDefault()
@@ -22,53 +22,51 @@ const InstallBanner = () => {
   return (
     visible && (
       <div id="install-banner">
-        <Sticky top={0}>
-          <Container>
-            <Banner setVisible={setVisible}>
-              <Box marginRight={6} flex="grow">
-                <Text color="white">
-                  Enjoy your songs offline using our free app!
-                </Text>
-              </Box>
-              <Box paddingX={3}>
-                <Button
-                  text="Install"
-                  inline
-                  onClick={e => {
-                    installEvent.prompt()
-                    installEvent.userChoice.then(choice => {
-                      if (choice.outcome === "accepted") {
-                          // trackCustomEvent({
-                          //     category: `Install Button`,
-                          //     label: `Installed`,
-                          //     action: "Click"
-                          // })
+        <Container>
+          <Banner setVisible={setVisible}>
+            <Box marginRight={6} flex="grow">
+              <Text color="white">
+                Enjoy your songs offline using our free app!
+              </Text>
+            </Box>
+            <Box paddingX={3}>
+              <Button
+                text="Install"
+                inline
+                onClick={e => {
+                  installEvent.prompt()
+                  installEvent.userChoice.then(choice => {
+                    if (choice.outcome === "accepted") {
+                      // trackCustomEvent({
+                      //     category: `Install Button`,
+                      //     label: `Installed`,
+                      //     action: "Click"
+                      // })
 
-                          if(window.gtag) {
-                              window.gtag('event', 'Installed', {
-                                  'event_category': 'Install Button',
-                              });
-                          }
-                      } else {
-                          // trackCustomEvent({
-                          //     category: `Install Button`,
-                          //     label: `Declined Installation`,
-                          //     action: "Click"
-                          // })
-
-                          if(window.gtag) {
-                              window.gtag('event', 'Installed', {
-                                  'event_category': 'Install Button',
-                              });
-                          }
+                      if (window.gtag) {
+                        window.gtag("event", "Installed", {
+                          event_category: "Install Button"
+                        })
                       }
-                    })
-                  }}
-                />
-              </Box>
-            </Banner>
-          </Container>
-        </Sticky>
+                    } else {
+                      // trackCustomEvent({
+                      //     category: `Install Button`,
+                      //     label: `Declined Installation`,
+                      //     action: "Click"
+                      // })
+
+                      if (window.gtag) {
+                        window.gtag("event", "Installed", {
+                          event_category: "Install Button"
+                        })
+                      }
+                    }
+                  })
+                }}
+              />
+            </Box>
+          </Banner>
+        </Container>
       </div>
     )
   )

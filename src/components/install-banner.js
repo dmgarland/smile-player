4c76053da9b42204d275c8546eb479e1c5e64282
Dan Garland <dan@dangarland.co.uk>
2021-03-01 22:42:33 +0000

    Dont show banner by default

--- src/components/install-banner.js
+++ src/components/install-banner.js
@@ -5,7 +5,7 @@ import { Container, Text, Button, Box } from "gestalt"
 
 const InstallBanner = () => {
   const [installEvent, setInstallEvent] = useState(null)
-  const [visible, setVisible] = useState(true)
+  const [visible, setVisible] = useState(false)
 
   const saveInstallEvent = e => {
     e.preventDefault()

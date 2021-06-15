ec69e5693bb38fc201deba5df9e768cc9c3520b1
Dan Garland <dan@dangarland.co.uk>
2021-06-15 16:35:05 +0100

    Update about-smiling-sessions.js

--- src/pages/about-smiling-sessions.js
+++ src/pages/about-smiling-sessions.js
@@ -45,14 +45,13 @@ const About = ({ data, location }) => {
 
               <p>
                 The sessions are filmed by our fabulous musicians and celebrity
-                guests, created as videos (with on screen lyrics) which we
+                guests as well as care home residents and staff and are created as videos (with on screen lyrics) which we
                 upload to the app every Monday. The videos are building a video
                 juke box of 15 songs a series that can be used over and over as
                 a download or for streaming.
               </p>
               <p>
-                Series 2 is now underway, taking us up to the festive season and
-                we hope you continue to enjoy singing the songs! Knowing how
+                In the last year we have completed three series. We hope that you will continue to enjoy singing the songs. Knowing how
                 singing can really impact on improving health and well-being,
                 while reducing stress, blood pressure and much more, we hope our
                 weekly singalong sessions will also increase the morale of

56898013286bf671f15ac273a76cf105db250543
Dan Garland <dan@dangarland.co.uk>
2022-12-02 20:23:36 +0000

    Adding stuff

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "userregistrations": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
